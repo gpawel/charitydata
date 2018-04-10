f83d1392cfb88d24367b8dd23ef429947eebf8c1
Pavel Gouchtchine <pavel.gouchtchine@cantire.com>
2018-04-10 10:59:17 -0400

    Testing extractions of years from finantial data

--- src/main/java/org/qamation/charities/extractor/FinInfoExtractor.java
+++ src/main/java/org/qamation/charities/extractor/FinInfoExtractor.java
@@ -12,20 +12,39 @@ public class FinInfoExtractor {
     private static final String SUMMARY_ELEMENT_XPATH = FIN_INFO_ELEMENT_XPATH+"/table[2]";
     private static final String SUMMARY_YEARS_XPATH = SUMMARY_ELEMENT_XPATH+"/thead/tr/th";
 
+    private WebDriver driver;
+
+    public FinInfoExtractor(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
     /*
     public static List<FinStats> extactFinInfo(WebDriver driver) {
         String[] years = getYears(driver);
     }
 */
-    public static String[] getYears(WebDriver driver) {
+    public String[] getYears() {
         By xpath = By.xpath(SUMMARY_YEARS_XPATH);
         List<WebElement> els = driver.findElements(xpath);
+        return getElementsContent(els);
+    }
+
+    public String[] get
+
+    private  String[] getElementsContent(List<WebElement> els) {
         if (els.size()==0) return new String[] {};
         String[] result = new String[els.size()];
         for (int i=0; i<result.length; i++) {
-            result[i] = els.get(i).getText();
+            result[i] = getText(els.get(i));
         }
         return result;
     }
+
+    private String getText(WebElement el) {
+        return el.getText();
+    }
+
+
 
 }
--- src/main/test/org/qamation/charities/extractor/FinInfoExtractorTests.java
+++ src/main/test/org/qamation/charities/extractor/FinInfoExtractorTests.java
@@ -1,16 +1,24 @@
 package org.qamation.charities.extractor;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FinInfoExtractorTests extends BaseTest {
     // https://charityintelligence.ca/charity-details/772-blue-sea-philanthropy
+    private FinInfoExtractor extractor;
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+        extractor = new FinInfoExtractor(driver);
+    }
 
     @Test
     public void extractYearsTest1() {
         page.openPage("https://charityintelligence.ca/charity-details/154-acclaim-health");
         page.isReady();
-        String[] data = FinInfoExtractor.getYears(driver);
+        String[] data = extractor.getYears();
         Assert.assertEquals("2016",data[0]);
         Assert.assertEquals("2015",data[1]);
         Assert.assertEquals("2014",data[2]);
@@ -20,7 +28,7 @@ public class FinInfoExtractorTests extends BaseTest {
     public void extractYearsTest2() {
         page.openPage("https://charityintelligence.ca/charity-details/772-blue-sea-philanthropy");
         page.isReady();
-        String[] data = FinInfoExtractor.getYears(driver);
+        String[] data = extractor.getYears();
         Assert.assertEquals("2015",data[0]);
         Assert.assertEquals("2014",data[1]);
         Assert.assertEquals("2013",data[2]);
